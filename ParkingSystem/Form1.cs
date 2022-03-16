2cecd1827bf8175f8634dc9cd4b835d046bc56e2
TOSUN <33561581+sungmanko@users.noreply.github.com>
2022-03-16 21:00:28 +0900

    初回説明データ

---
+++ ParkingSystem/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ParkingSystem
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// clsUtilクラスインスタンス
+        /// </summary>
+        private clsUtil cu = new clsUtil();
+
+        /// <summary>
+        /// コンストラクター
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 画面ロードイベント処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 初期起動時
+            button2.Enabled = false;
+        }
+
+        /// <summary>
+        /// ボタン押下イベント処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // ボタン押下イベント処理
+            button2.Enabled = true;
+        }
+
+        /// <summary>
+        /// 駐車料金計算
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // 金額計算
+            textBox2.Text = cu.ParkingPrice(textBox1.Text);
+        }
+    }
+}
---
+++ ParkingSystem/clsUtil.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ParkingSystem
+{
+    public class clsUtil
+    {
+        private int BaseTime = 10;
+        private int BasePrice = 300;
+
+        /// <summary>
+        /// 駐車料金計算
+        /// </summary>
+        /// <returns>計算結果金額</returns>
+        public string ParkingPrice(string targetData)
+        {
+            return
+                (Math.Ceiling(Convert.ToDouble(targetData) / BaseTime) * BasePrice).ToString();
+        }
+
+        /// <summary>
+        /// 駐車料金計算
+        /// </summary>
+        /// <returns>計算結果金額</returns>
+        public string ParkingPriceMinus(string targetData)
+        {
+            return
+                (Math.Ceiling(Convert.ToDouble(targetData) / BaseTime) * BasePrice).ToString();
+        }
+    }
+}
